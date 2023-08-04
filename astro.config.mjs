bd4b986473d8e1b01baddf383331023ebbde679a
Ribbit <echo07200503@gmail.com>
2023-08-04 17:57:52 +0900

    🔥 tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from 'astro/config';
 import partytown from '@astrojs/partytown';
-import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [
-    tailwind(),
     partytown({
       config: {
         forward: ['dataLayer.push'],
